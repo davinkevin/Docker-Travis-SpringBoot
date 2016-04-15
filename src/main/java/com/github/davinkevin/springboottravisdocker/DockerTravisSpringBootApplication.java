d81ce1688800f4107f80f873ce31734087d7aeed
Davin Kevin <davin.kevin@gmail.com>
2016-04-15 18:03:12 +0200

    feat(): add poc application

---
+++ src/main/java/com/github/davinkevin/springboottravisdocker/DockerTravisSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.github.davinkevin.springboottravisdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerTravisSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerTravisSpringBootApplication.class, args);
+	}
+}
